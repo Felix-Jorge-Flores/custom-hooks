a409aa9448cdda018d9da13a358db35999da72d1
Felix <felixandjorge08@gmail.com>
2022-08-14 15:13:41 -0500

    Primer commit

---
+++ useTodos.js
@@ -0,0 +1,80 @@
+import { useEffect, useReducer } from "react"
+import { todoReducer } from "../08-useReducer/todoReducer"
+
+const initialState = [
+    //     {
+    //     id: new Date().getTime(),
+    //     description: 'Recolectar la piedra del alma',
+    //     done: false,
+
+    // },
+    // {
+    //     id: new Date().getTime() * 3,
+    //     description: 'Recolectar la piedra del tiempo',
+    //     done: false,
+
+    // },
+];
+
+
+const init = () => {
+    try {
+        return JSON.parse(localStorage.getItem('todos') || []);
+    } catch (error) {
+        console.log('Error', error);
+    }
+}
+
+
+
+export const useTodos = () => {
+
+    const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos))
+    }, [todos])
+
+
+    const handleNewTodo = (todo) => {
+        const action = {
+            type: '[TODO] Add Todo',
+            payload: todo
+        }
+
+        dispatch(action);
+        console.log(todo);
+
+    }
+
+    const handleDeleteTodo = (id) => {
+
+        dispatch({
+            type: '[TODO] Remove todo',
+            payload: id
+        });
+
+    }
+
+    const handleToggleTodo = (id) => {
+        console.log({ id });
+
+        dispatch({
+            type: '[TODO] Toggle Todo',
+            payload: id
+        });
+
+    }
+
+
+    return {
+        todos,
+        handleNewTodo,
+        handleDeleteTodo,
+        handleToggleTodo,
+        todosCount: todos.length,
+        pendingTodosCount: todos.filter(todo => !todo.done).length
+    }
+
+}
